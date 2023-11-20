6b05e14c547bcc47f5f7171e5e24d546534c31b1
geovani <nilsongeovanimoranlue@gmail.com>
2023-11-19 18:11:06 -0600

    Nuevo FizzBuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,23 @@
+
+      
+def numeros():
+    for numero in range(1,101):
+        if numero % 3 ==0 and numero % 5 ==0:
+         print("FizzBuzz")
+        elif numero % 3 ==0:
+            print ("Fizz")
+        elif numero % 5 ==0:
+            print("Buzz")
+        else:
+            print(numero)
+numeros()
+
+     
+
+
+
+
+
+
+
+
